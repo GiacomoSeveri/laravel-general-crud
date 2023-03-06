29eeb0b17190e195059180a16ec6d258930141f3
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-06 13:00:13 +0100

    added seeder

---
+++ config/cards.php
@@ -0,0 +1,72 @@
+<?php 
+return 
+[
+    [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+    ],
+        [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+    ],
+        [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+    ],
+        [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+    ],
+        [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+    ],
+        [
+        "name_card" => "nome",
+        "coast" => "5",
+        "img" => "https://www.artofmtg.com/wp-content/uploads/2023/01/Distorted-Curiosity-Phyrexia-All-Will-Be-One-MtG-Art.jpg",
+        "type_card" => "type",
+        "ed" => "ed",
+        "effect" => "scarta carte pesca carte",
+        "text" => "loremdsfdgfhmgfdtghfjnmdyftshnmdfvugjharnsyhd",
+        "force" => "5",
+        "costitution" => "4"
+        ]
+
+
+        ];
